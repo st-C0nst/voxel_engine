c3f3cb16b022aa5c33b60ee953f1f77f1bad87ac
Nicholas Costanza <nicholas.costanz@gmail.com>
2026-03-31 21:56:59 -0400

    removed buggy code

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,5 @@
-use voxel_engine::{Player, KeyState};
-use glam::Vec3;
 
 fn main() {
     // Example usage of the player controller
-    let mut player = Player::new(Vec3::new(0.0, 50.0, 0.0), 16.0 / 9.0);
-    
-    println!("Player initialized at position: {:?}", player.position);
-    println!("Camera forward direction: {:?}", player.camera.forward());
-    
-    // Example: Simulate one frame of movement
-    let mut key_state = KeyState::default();
-    key_state.forward = true;
-    key_state.up = true;
-    
-    let delta_time = 1.0 / 60.0; // 60 FPS
-    player.update(key_state, delta_time);
-    
-    println!("Player position after update: {:?}", player.position);
-    println!("Player velocity: {:?}", player.velocity);
+    print!("Hello, world!")
 }
