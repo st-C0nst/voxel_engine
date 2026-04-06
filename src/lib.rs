cee225df0887439d60887a76c55b5fb31120340c
Nicholas Costanza <nicholas.costanz@gmail.com>
2026-04-06 00:02:05 -0400

    stuff

--- src/lib.rs
+++ src/lib.rs
@@ -160,6 +160,69 @@ pub struct State {
   mouse_pressed: bool
 }
 
+fn create_render_pipeline(
+    device: &wgpu::Device,
+    layout: &wgpu::PipelineLayout,
+    color_format: wgpu::TextureFormat,
+    depth_format: Option<wgpu::TextureFormat>,
+    vertex_layouts: &[wgpu::VertexBufferLayout],
+    shader: wgpu::ShaderModuleDescriptor,
+  ) -> wgpu::RenderPipeline {
+  let shader = device.create_shader_module(shader);
+
+  device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
+    label: Some(&format!("{:?}", shader)),
+    layout: Some(layout),
+    vertex: wgpu::VertexState {
+      module: &shader,
+      entry_point: Some("vs_main"),
+      buffers: vertex_layouts,
+      compilation_options: Default::default(),
+    },
+    fragment: Some(wgpu::FragmentState {
+      module: &shader,
+      entry_point: Some("fs_main"),
+      targets: &[Some(wgpu::ColorTargetState {
+        format: color_format,
+        blend: Some(wgpu::BlendState {
+          alpha: wgpu::BlendComponent::REPLACE,
+          color: wgpu::BlendComponent::REPLACE,
+        }),
+        write_mask: wgpu::ColorWrites::ALL,
+      })],
+      compilation_options: Default::default(),
+    }),
+    primitive: wgpu::PrimitiveState {
+      topology: wgpu::PrimitiveTopology::TriangleList,
+      strip_index_format: None,
+      front_face: wgpu::FrontFace::Ccw,
+      cull_mode: Some(wgpu::Face::Back),
+      // Setting this to anything other than Fill requires Features::NON_FILL_POLYGON_MODE
+      polygon_mode: wgpu::PolygonMode::Fill,
+      // Requires Features::DEPTH_CLIP_CONTROL
+      unclipped_depth: false,
+      // Requires Features::CONSERVATIVE_RASTERIZATION
+      conservative: false,
+    },
+    depth_stencil: depth_format.map(|format| wgpu::DepthStencilState {
+      format,
+      depth_write_enabled: Some(true),
+      depth_compare: Some(wgpu::CompareFunction::Less),
+      stencil: wgpu::StencilState::default(),
+      bias: wgpu::DepthBiasState::default(),
+    }),
+    multisample: wgpu::MultisampleState {
+      count: 1,
+      mask: !0,
+      alpha_to_coverage_enabled: false,
+    },
+    // If the pipeline will be used with a multiview render pass, this
+    // tells wgpu to render to just specific texture layers.
+    multiview_mask: None,
+    cache: None,
+  })
+}
+
 impl State {
 }
 
@@ -199,11 +262,9 @@ impl ApplicationHandler<State> for App {
     _device_id: DeviceId,
     event: DeviceEvent,
   ) {
-    // if the state is not set for app, then we should exit
-    let state = if let Some(state) = &mut self.state {
-      state
-    } else {
-      return;
+    let state = match &mut self.state {
+      Some(state) => state,
+      None => return,
     };
 
     match event {
